7c26cceca41b9b1fc4f0ddf99797535c6455f172
Sam Martin <broccoliboy@gmail.com>
2023-06-09 22:19:09 -0500

    add uptime sensor, change light config, invert indicator

--- components/gosund_sw2/light.py
+++ components/gosund_sw2/light.py
@@ -1,13 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, light, output
-from esphome.const import CONF_OUTPUT_ID
+from esphome.const import CONF_ID, CONF_OUTPUT_ID
 
 DEPENDENCIES = ['uart']
 INDICATOR_KEY = "indicator"
 
 gosund_sw2_ns = cg.esphome_ns.namespace('gosund_sw2')
-GosundSW2 = gosund_sw2_ns.class_('GosundSW2', light.LightOutput, uart.UARTDevice)
+GosundSW2 = gosund_sw2_ns.class_('GosundSW2', light.LightOutput, uart.UARTDevice, cg.Component)
 
 CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend({
     cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(GosundSW2),
@@ -18,11 +18,10 @@ CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend({
 def to_code(config):
     print("config:", config)
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
     yield light.register_light(var, config)
 
     if INDICATOR_KEY in config:
         indicator = yield cg.get_variable(config[INDICATOR_KEY])
         cg.add(var.set_indicator(indicator))
-    
-    cg.add(cg.App.register_component(var))
